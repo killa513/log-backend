732474fa556883f4135c31338156675d21100d67
killa513 <dengidengi@protonmail.com>
2025-10-23 14:31:54 +0500

    first commit

--- src/config/db.php
+++ src/config/db.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=myapp;charset=utf8mb4", "root", "");
+$pdo = new PDO("mysql:host=localhost;dbname=myapp;charset=utf8mb4", "web", "webpass");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 return $pdo;
