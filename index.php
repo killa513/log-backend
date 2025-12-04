5242768d43ad617fe7a51851099cf482ada87ff5
killa513 <dengidengi@protonmail.com>
2025-12-04 22:49:21 +0500

    test

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
-<?php
+    <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
-use App\Controllers\BaseController;
+    use Slim\Factory\AppFactory;
+    use App\Controllers\BaseController;
 
-$pdo = require __DIR__ . '/src/config/db.php';
-BaseController::setPDO($pdo);
+    $pdo = require __DIR__ . '/src/config/db.php';
+    BaseController::setPDO($pdo);
 
-$app = AppFactory::create();
-$app->addBodyParsingMiddleware();
-$app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, true, true);
+    $app = AppFactory::create();
+    $app->setBasePath('/api');
+    $app->addBodyParsingMiddleware();
+    $app->addRoutingMiddleware();
+    $app->addErrorMiddleware(true, true, true);
 
-require __DIR__ . '/src/routes.php';
+    require __DIR__ . '/src/routes.php';
 
-$app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-});
+    $app->add(function ($request, $handler) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    });
 
-$app->run();
+    $app->run();    
